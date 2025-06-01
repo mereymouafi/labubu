4dbe8d277469343fed17a5f4f937d57625f81560
MERYEM OUAFI <169082030+mereymouafi@users.noreply.github.com>
2025-06-01 14:13:41 +0100

    sqdqsdf

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import HeroBanner from '../components/Home/HeroBanner';
 import NewArrivals from '../components/Home/NewArrivals';
-import FeaturedCollections from '../components/Home/FeaturedCollections';
 import FeaturedProducts from '../components/Home/FeaturedProducts';
 import TrendingProducts from '../components/Home/TrendingProducts';
+import OnSaleProducts from '../components/Home/OnSaleProducts';
 
 const Home: React.FC = () => {
   return (
     <div className="pt-24">
       <HeroBanner />
       <NewArrivals />
-      <FeaturedCollections />
       <FeaturedProducts />
+      <OnSaleProducts />
       <TrendingProducts />
     </div>
   );
