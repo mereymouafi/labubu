d1bf3d6b0377534cd2c74e5d1df234c94bdda534
MERYEM OUAFI <169082030+mereymouafi@users.noreply.github.com>
2025-06-08 21:56:24 +0100

    cheJGRThRgrtljrjhjgkjkhguyjhyefdsczrtgdfggertrtgggj

--- src/pages/NewAndFeaturedPage.tsx
+++ src/pages/NewAndFeaturedPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import NewAndFeatured from '../components/Home/NewAndFeatured';
+import NewArrivals from '../components/Home/NewArrivals';
+import Featured from '../components/Home/Featured';
 
 const NewAndFeaturedPage: React.FC = () => {
   return (
     <div className="container mx-auto px-4 md:px-8 lg:px-16 xl:px-24 mt-24">
-      
-      <NewAndFeatured />
+      <h1 className="text-3xl font-bold mb-8">New & Featured</h1>
+      <NewArrivals />
+      <Featured />
     </div>
   );
 };
